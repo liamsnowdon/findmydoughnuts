2f79102831b40499e46cf40767f319bbcc9ca7c3
Liam Snowdon <snowdon.liam@gmail.com>
2018-08-09 17:28:02 +0100

    FIND-9: add user icon for geolocation FIND-12: remove jQuery from app and refactor JS.

--- assets/js/doughnut-search.js
+++ assets/js/doughnut-search.js
@@ -2,118 +2,42 @@ var infoWindow;
 var map;
 var geocoder;
 
-$(document).ready(function() {
+var DoughnutApp = {
+    elements: {
+        doughnutPopup: document.querySelector('.js-doughnut-popup'),
 
-    $('.js-search-myself').click(function () {
-        $('.doughnut-popup__search').slideDown();
-        if (!$(this).hasClass('active')) {
-            $('.button').removeClass('active');
-            $(this).addClass('active');
-        }
-    });
+        searchForm: document.querySelector('#location-search'),
+        searchContainer: document.querySelector('.js-search-location'),
+        searchButton: document.querySelector('.js-search-myself'),
+        searchField: document.querySelector('.js-search-field'),
+        searchErrorMessage: document.querySelector('.js-search-error'),
 
-});
+        userSearchButton: document.querySelector('.js-search-myself'),
+        userFind: document.querySelector('.js-find-me'),
 
-function initMap() {
+        map: document.querySelector('#map'),
+        mapPopup: document.querySelector('.js-map-popup')
+    },
+    methods: {
+        removeButtonActiveStates: function() {
+            var buttons = document.querySelectorAll('.button');
 
-    // Set default settings of map before geolocation kicks in
-    var defaultLocation = {lat: 51.5210727, lng: -0.2444021};
+            for (var i = 0; i < buttons.length; i++) {
+                buttons[i].classList.remove('active')
+            }
+        },
+        closePopup: function() {
+            var pageOverlay = document.querySelector('.page-overlay');
 
-    geocoder = new google.maps.Geocoder();
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: defaultLocation,
-        zoom: 3
-    });
+            DoughnutApp.elements.map.classList.remove('inactive');
+            pageOverlay.remove();
+            DoughnutApp.elements.doughnutPopup.remove();
+        },
+        geolocationSuccess: function(currentLocation) {
+            var currentLocationCoords = currentLocation.coords;
+            var userLocation = new google.maps.LatLng(currentLocationCoords.latitude, currentLocationCoords.longitude);
 
-    $('.js-find-me').click(function () {
-        $('.button').removeClass('active');
-        $(this).addClass('active');
-        $('.doughnut-popup').append('<div class="loading-overlay"><img class="loading-overlay__spinner" src="assets/img/loading-spinner.png" alt="Loading spinner" aria-label="hidden" /></div>');
-        navigator.geolocation.getCurrentPosition(success, error);
-    });
-
-    function success(currentLocation) {
-
-        var currentLocationCoords = currentLocation.coords;
-        var userLocation = new google.maps.LatLng(currentLocationCoords.latitude, currentLocationCoords.longitude);
-
-        map.setCenter(userLocation);
-        map.setZoom(15);
-
-        infoWindow = new google.maps.InfoWindow({
-            maxWidth: '250'
-        });
-
-        var request = {
-            location: userLocation,
-            radius: '500',
-            query: 'doughnuts'
-        };
-
-        var userMarker = new google.maps.Marker({
-            position: userLocation,
-            map: map,
-            icon: 'assets/img/user.png'
-        });
-
-        var service = new google.maps.places.PlacesService(map);
-        service.textSearch(request, callback);
-
-        closePopup();
-    }
-
-    function error() {
-        alert("Awww, geolocation is not enabled on your device :(\nTry searching with an address instead.");
-        $('.doughnut-popup__search').slideDown();
-        $('#doughnut-popup__field').focus();
-        $('.js-search-myself').addClass('active');
-        $('.loading-overlay').remove();
-        $('.js-find-me').removeClass('active');
-        $('.js-find-me').addClass('disabled');
-        $('.js-find-me').blur();
-    }
-
-}
-
-function callback(results, status) {
-    if (status === google.maps.places.PlacesServiceStatus.OK) {
-        for (var i = 0; i < results.length; i++) {
-            var place = results[i];
-            createMarker(results[i]);
-        }
-    }
-}
-
-function createMarker(place) {
-    var placeLoc = place.geometry.location;
-    var doughnutIcon = 'favicon.ico';
-
-    var marker = new google.maps.Marker({
-        map: map,
-        position: place.geometry.location,
-        icon: doughnutIcon
-    });
-
-    google.maps.event.addListener(marker, 'click', function() {
-
-        infoWindow.setContent('<div><strong>' + place.name + '</strong><br>' +
-            place.formatted_address + '</div>');
-        infoWindow.open(map, this);
-    });
-}
-
-function codeAddress() {
-    var address = document.getElementById('doughnut-popup__field').value;
-
-    if (address === '') {
-        alert("Please enter a location to search.");
-        return;
-    }
-
-    geocoder.geocode( { 'address': address}, function(results, status) {
-        if (status === 'OK') {
-            var userLocation = results[0].geometry.location;
-            map.setCenter(results[0].geometry.location);
+            map.setCenter(userLocation);
             map.setZoom(15);
 
             infoWindow = new google.maps.InfoWindow({
@@ -129,29 +53,137 @@ function codeAddress() {
             var userMarker = new google.maps.Marker({
                 position: userLocation,
                 map: map,
-                icon: 'assets/img/user.png'
+                icon: DoughnutApp.images.userIcon
             });
 
             var service = new google.maps.places.PlacesService(map);
-            service.textSearch(request, callback);
+            service.textSearch(request, DoughnutApp.methods.callback);
 
-            $('.map-popup').show();
-            closePopup();
+            DoughnutApp.methods.closePopup();
+        },
+        geolocationError: function() {
+            alert("Awww, geolocation is not enabled on your device :(\nTry searching with an address instead.");
 
-        } else {
-            $('.error-message').html('Geocode was not successful for the following reason: ' + status);
-            $('.error-message').css('display', 'block');
+            var loadingOverlay = document.querySelector('.loading-overlay');
+
+            DoughnutApp.elements.searchContainer.style.display = 'block';
+            DoughnutApp.elements.searchField.focus();
+            DoughnutApp.elements.userSearchButton.classList.add('active');
+            loadingOverlay.remove();
+            DoughnutApp.elements.userFind.classList.remove('active');
+            DoughnutApp.elements.userFind.classList.add('disabled');
+            DoughnutApp.elements.userFind.blur();
+        },
+        callback: function(results, status) {
+            if (status === google.maps.places.PlacesServiceStatus.OK) {
+                for (var i = 0; i < results.length; i++) {
+                    var place = results[i];
+                    DoughnutApp.methods.createMarker(results[i]);
+                }
+            }
+        },
+        createMarker: function(place) {
+            var placeLoc = place.geometry.location;
+            var doughnutIcon = DoughnutApp.images.doughnutIcon;
+
+            var marker = new google.maps.Marker({
+                map: map,
+                position: place.geometry.location,
+                icon: doughnutIcon
+            });
+
+            google.maps.event.addListener(marker, 'click', function() {
+
+                infoWindow.setContent('<div><strong>' + place.name + '</strong><br>' +
+                    place.formatted_address + '</div>');
+                infoWindow.open(map, this);
+            });
+        },
+        codeAddress: function() {
+            var address = document.getElementById('doughnut-popup__field').value;
+
+            if (address === '') {
+                alert("Please enter a location to search.");
+                return;
+            }
+
+            geocoder.geocode( { 'address': address}, function(results, status) {
+                if (status === 'OK') {
+                    var userLocation = results[0].geometry.location;
+                    map.setCenter(results[0].geometry.location);
+                    map.setZoom(15);
+
+                    infoWindow = new google.maps.InfoWindow({
+                        maxWidth: '250'
+                    });
+
+                    var request = {
+                        location: userLocation,
+                        radius: '500',
+                        query: 'doughnuts'
+                    };
+
+                    var userMarker = new google.maps.Marker({
+                        position: userLocation,
+                        map: map,
+                        icon: DoughnutApp.images.userIcon
+                    });
+
+                    var service = new google.maps.places.PlacesService(map);
+                    service.textSearch(request, DoughnutApp.methods.callback);
+
+                    DoughnutApp.elements.mapPopup.style.display = 'block';
+                    DoughnutApp.methods.closePopup();
+
+                } else {
+                    DoughnutApp.elements.searchErrorMessage.innerText = 'Geocode was not successful for the following reason: ' + status;
+                    DoughnutApp.elements.searchErrorMessage.style.display = 'block';
+                }
+            });
         }
+    },
+    images: {
+        loadingSpinner: 'assets/img/doughnut.png',
+        doughnutIcon: 'assets/img/doughnut-icon.png',
+        userIcon: 'assets/img/user-icon.png'
+    }
+};
+
+function initMap() {
+    // Set default settings of map before geolocation kicks in
+    var defaultLocation = {lat: 51.5210727, lng: -0.2444021};
+
+    geocoder = new google.maps.Geocoder();
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: defaultLocation,
+        zoom: 3
     });
 }
 
-$("#location-search").submit(function(e) {
-    e.preventDefault();
-    codeAddress();
+
+// Event listeners
+DoughnutApp.elements.searchForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+    DoughnutApp.methods.codeAddress();
 });
 
-function closePopup() {
-    $('#map').removeClass('inactive');
-    $('.page-overlay').remove();
-    $('.doughnut-popup').fadeOut();
-}
+DoughnutApp.elements.searchButton.addEventListener('click', function() {
+    DoughnutApp.elements.searchContainer.style.display = 'block';
+    if (!this.classList.contains('active')) {
+        DoughnutApp.methods.removeButtonActiveStates();
+        this.classList.add('active');
+    }
+});
+
+DoughnutApp.elements.userFind.addEventListener('click', function() {
+    var doughnutPopup = document.querySelector('.js-doughnut-popup');
+    var loadingOverlay = document.createElement('div');
+
+    loadingOverlay.innerHTML = '<div class="loading-overlay"><img class="loading-overlay__spinner" src="' + DoughnutApp.images.loadingSpinner + '" alt="" /></div>';
+
+    DoughnutApp.methods.removeButtonActiveStates();
+    this.classList.add('active');
+
+    doughnutPopup.appendChild(loadingOverlay);
+    navigator.geolocation.getCurrentPosition(DoughnutApp.methods.geolocationSuccess, DoughnutApp.methods.geolocationError);
+});
